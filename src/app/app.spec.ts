097e9f5dcb162ce7b79f09772ff4e8ab3013d173
KD63799 <aninach.beg@gmail.com>
2026-04-28 11:14:21 +0200

    created fews ui components to test lint and added some style

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -2,22 +2,71 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let app: App;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
     }).compileComponents();
+
+    const fixture = TestBed.createComponent(App);
+    app = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
+  it('should create', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TP-Task');
+  it('should add a task', () => {
+    app.newTitle = 'Buy milk';
+    app.addTask();
+
+    expect(app.tasks().length).toBe(1);
+    expect(app.tasks()[0].title).toBe('Buy milk');
+    expect(app.tasks()[0].done).toBe(false);
+  });
+
+  it('should not add a blank task', () => {
+    app.newTitle = '   ';
+    app.addTask();
+
+    expect(app.tasks().length).toBe(0);
+  });
+
+  it('should toggle a task done then undone', () => {
+    app.newTitle = 'Write tests';
+    app.addTask();
+    const id = app.tasks()[0].id;
+
+    app.toggleTask(id);
+    expect(app.tasks()[0].done).toBe(true);
+
+    app.toggleTask(id);
+    expect(app.tasks()[0].done).toBe(false);
+  });
+
+  it('should delete a task', () => {
+    app.newTitle = 'Task to delete';
+    app.addTask();
+    const id = app.tasks()[0].id;
+
+    app.deleteTask(id);
+
+    expect(app.tasks().length).toBe(0);
+  });
+
+  it('should compute pending and done counts correctly', () => {
+    app.newTitle = 'Task A';
+    app.addTask();
+    app.newTitle = 'Task B';
+    app.addTask();
+    app.newTitle = 'Task C';
+    app.addTask();
+
+    app.toggleTask(app.tasks()[0].id);
+    app.toggleTask(app.tasks()[1].id);
+
+    expect(app.doneCount()).toBe(2);
+    expect(app.pendingCount()).toBe(1);
   });
 });
